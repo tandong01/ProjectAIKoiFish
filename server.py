1e45702ea9db06f1d69d4906314fb52d85e55321
tandong01 <105887276+tandong01@users.noreply.github.com>
2022-06-22 02:14:36 +0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,39 @@
+# Đoạn code này không cần quan tâm có chạy được trên python hay không, vì nó sẽ chạy trực tiếp trên github, do trên python chưa cài streamlit
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from streamlit_option_menu import option_menu
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+from PIL import Image
+st.write("""
+         # PHÂN LOẠI 18 LOẠI CÁ KOI
+         """
+         )
+model = tf.keras.models.load_model("model_cc.h5") #model m train
+
+### load file
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg","jpeg","png"])
+
+classes = ['Kohaku', 'Ginrin', 'Goshiki', 'Hikarimuji', 'Hikarimoyo', 'Kumonryu', 'Kujaku', 'Doitsu', 'Chagoi',
+               'Ochiba', 'Taisho Sanke', 'Showa ', 'Utsuri', 'Bekko', 'Asagi', 'Shusui', 'Tancho', 'Goromo']
+ 
+if uploaded_file is not None:
+    # Convert the file
+    img = image.load_img(uploaded_file,target_size=(224,224)) 
+    image = Image.open(uploaded_file)
+    st.image(image, use_column_width=True)
+    img = img_to_array(img)
+    img = img.reshape(1,224,224,3)
+    img = img.astype('float32')
+    img = img/255
+        
+    #Button: nút dự đoán sau khi up ảnh
+    Genrate_pred = st.button("Dự đoán") 
+    
+    if Genrate_pred:
+    
+        prediction = model.predict(img)
+        st.write("""Kết quả dự đoán của hình này là: {}""".format(classes [np.argmax(prediction[0])])) 
+        st.write("Độ chính xác là: {:.2f} %".format(100*np.max(prediction[0],axis=0)))
