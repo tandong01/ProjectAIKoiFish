1632d6e1231bf51c2214c172e5081e6267e798b7
tandong01 <105887276+tandong01@users.noreply.github.com>
2022-06-22 12:06:05 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -11,11 +11,12 @@ st.header("""
          # PHÂN LOẠI 18 LOẠI CÁ KOI
          """
          )
-model = tf.keras.models.load_model("model_cc.h5") #model m train
+st.write("18 loại cá Koi bao gồm các loại: Kohaku, Ginrin, Goshiki', Hikarimuji, Hikarimoyo, Kumonryu, Kujaku, Doitsu, Chagoi,Ochiba, Taisho Sanke, Showa , Utsuri, Bekko, Asagi, Shusui, Tancho, Goromo ")
+model = tf.keras.models.load_model("model_cc.h5")
 
 ### load file
 uploaded_file = st.file_uploader("Choose an image file", type=["jpg","jpeg","png"])
-video = st.file_uploader("Choose an video file", type=["mp4"])
+
 
 classes = ['Kohaku', 'Ginrin', 'Goshiki', 'Hikarimuji', 'Hikarimoyo', 'Kumonryu', 'Kujaku', 'Doitsu', 'Chagoi',
                'Ochiba', 'Taisho Sanke', 'Showa ', 'Utsuri', 'Bekko', 'Asagi', 'Shusui', 'Tancho', 'Goromo']
@@ -39,44 +40,4 @@ if uploaded_file is not None:
         st.write("""Kết quả dự đoán của hình này là: {}""".format(classes [np.argmax(prediction[0])])) 
         st.write("Độ chính xác là: {:.2f} %".format(100*np.max(prediction[0],axis=0)))
 
-if video is not None:
-    cap = cv2.VideoCapture(video)
-    while(True):
-        # Capture frame-by-frame
-        #
-        ret, image_org = cap.read()
-        if not ret:
-            continue
-        image_org = cv2.resize(image_org, dsize=None,fx=0.5,fy=0.5)
-        # Resize
-        image = image_org.copy()
-        image = cv2.resize(image, dsize=(224, 224))
-        image = image.astype('float')*1./255
-        # Convert to tensor
-        image = np.expand_dims(image, axis=0)
 
-        # Predict
-        predict = model.predict(image)
-        print("This picture is: ", classes[np.argmax(predict[0])])
-        print(np.max(predict[0],axis=0))
-            # Show image
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        org = (50, 50)
-        fontScale = 1
-        color = (0, 255, 0)
-        thickness = 2
-        if (np.max(predict)>=0.3) and (np.argmax(predict[0])!=0):
-
-            cv2.putText(image_org, classes[np.argmax(predict)]+': '+ str(np.max(predict[0],axis=0)), org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
-        else:
-            cv2.putText(image_org,'Unknow', org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
-        cv2.imshow("Picture", image_org)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
